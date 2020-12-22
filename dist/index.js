9012061a85be8b81b0fa52e5187830a24ecd6fae
Christina Williams <christinaxtwilliams@gmail.com>
2020-12-21 20:22:06 -0500

    added code

--- dist/index.js
+++ dist/index.js
@@ -125,7 +125,7 @@ const html = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <h1>A JavaScript project</h1>
+  <h1>A Rainbow World</h1>
 
   <button>Stop Color Change</button>
   <div class="container">
