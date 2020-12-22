9b595c79633bcf8f46bce482e7bdeaba2687d2b1
Christina Williams <christinaxtwilliams@gmail.com>
2020-12-21 19:48:42 -0500

    added code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,4 @@
-const express = require('express');
-
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>A JavaScript project</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-  <h1>A JavaScript project</h1>
-</body>
-</html>`;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/html');
-  res.status(200).send(html);
-});
-
-module.exports = app;
+const changeThings = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      // console.log('The Interval');
