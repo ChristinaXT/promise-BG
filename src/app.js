4dc0e0a6e8fd492f54d780f71b5a40b99abb3ced
Christina Williams <christinaxtwilliams@gmail.com>
2020-12-21 19:49:10 -0500

    added code

--- src/app.js
+++ src/app.js
@@ -2,3 +2,10 @@ const changeThings = (color, delay) => {
   return new Promise((resolve, reject) => {
     const interval = setInterval(() => {
       // console.log('The Interval');
+
+      document.querySelector('.container').innerHTML = `<p>Hello</p>`;
+
+            // alert('Hello')
+            document.body.style.background = color
+            resolve();
+          }, delay);
